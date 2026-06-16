3ad384e296f5ec631e5cd13352413416d061bf00
Vanish <241622833+corruptedbytecode@users.noreply.github.com>
2026-06-16 14:29:37 +0100

    sync: char.lua

--- Scripts/char.lua
+++ Scripts/char.lua
@@ -3,5 +3,4 @@ local c = p.Character or p.CharacterAdded:Wait()
 local id = "rbxassestid://12307583853"
 
 for _, v in ipairs(c:GetDescendants()) do
-    if  v:IsA("BasePart")
- andend
+    if  v:IsA("BasePart")end
