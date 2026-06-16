ccc6a8e96eb516095ffea77401100c0b7c45d7c4
Vanish <241622833+corruptedbytecode@users.noreply.github.com>
2026-06-16 14:29:20 +0100

    sync: char.lua

--- Scripts/char.lua
+++ Scripts/char.lua
@@ -2,4 +2,6 @@ local p = game.Players.LocalPlayer
 local c = p.Character or p.CharacterAdded:Wait()
 local id = "rbxassestid://12307583853"
 
-for _, v in ipairs(c:GetDescendants())
+for _, v in ipairs(c:GetDescendants()) do
+    
+end
