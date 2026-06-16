c537455d57ad7a3dff6efde3eaa717a29e933b5d
Vanish <241622833+corruptedbytecode@users.noreply.github.com>
2026-06-16 14:31:01 +0100

    sync: char.lua

--- Scripts/char.lua
+++ Scripts/char.lua
@@ -1,9 +1,29 @@
 local p = game.Players.LocalPlayer
+
 local c = p.Character or p.CharacterAdded:Wait()
-local id = "rbxassestid://12307583853"
+
+local id = "rbxassetid://
+
+12307583853
+
+"
 
 for _, v in ipairs(c:GetDescendants()) do
-    if  v:IsA("BasePart") and v.Name = "HumanoidRootPart" then
-        for _, 
+
+    if v:IsA("BasePart") and v.Name ~= "HumanoidRootPart" then
+
+        for _, f in ipairs(Enum.NormalId:GetEnumItems()) do
+
+            local d = Instance.new("Decal")
+
+            d.Texture = id
+
+            d.Face = f
+
+            d.Parent = v
+
+        end
+
     end
+
 end
